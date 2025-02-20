e5f192e68b9f28d9a186f22b645305e58e8d6f30
dokku ayyappa <ayyappadokku89@gmail.com>
2025-02-20 14:13:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ st.title("Flat Price Prediction in Hyderabad")
 bedrooms = st.number_input("Number of Bedrooms", min_value=1, max_value=10, value=2)
 bathrooms = st.number_input("Number of Bathrooms", min_value=1, max_value=10, value=2)
 kitchens = st.number_input("Number of Kitchens", min_value=1, max_value=5, value=1)
-square_feet = st.number_input("Square Feet", min_value=500, max_value=10000, value=1000)
+square_feet = st.number_input("Square Feet", min_value=500, max_value=100000, value=1000)
 car_parking = st.selectbox("Car Parking", ["Yes", "No"])
 floor_number = st.number_input("Floor Number", min_value=1, max_value=50, value=1)
 age_of_property = st.number_input("Age of Property (years)", min_value=0, max_value=100, value=5)
-location = st.selectbox("Location", ["A", "B", "C", "D", "E"])
+location = st.selectbox("Location", ["Hitech City", "Kukatpally", "Gachibowli", "Banjara Hills", "Mehdipatnam","Madhapur","Begumpet","Jubilee Hills","Kondapur","Manikonda"])
 
 # Prediction
 if st.button("Predict Price"):
